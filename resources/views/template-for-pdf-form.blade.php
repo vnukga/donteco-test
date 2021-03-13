9345dbfef17d729b1dbcd069397bc390267542c7
Denis Korablev <d.korablev.dev@gmail.com>
2021-03-13 17:38:01 +0300

    Валидация формы с ФИО и датой.

--- resources/views/template-for-pdf-form.blade.php
+++ resources/views/template-for-pdf-form.blade.php
@@ -11,6 +11,15 @@
 
 </head>
 <body class="antialiased">
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 <form method="post">
     @csrf
     <ul>
